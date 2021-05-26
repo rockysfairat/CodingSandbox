c8f01f761f10c1733ab941847c3f2b690c973a42
Oleksandr Yurchenko <rockysfairat@gmail.com>
2021-05-26 22:18:47 +0200

    added 07

--- VaJS projects/07 - questions/script.js
+++ VaJS projects/07 - questions/script.js
@@ -1,10 +1,26 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
 
-// traversing the dom
-const btns = document.querySelectorAll('.question-btn');
-
-btns.forEach( (btn) => {
-    btn.addEventListener('click', (e) => {
-        console.log(e.currentTarget.parentElement.parentElement);
+questions.forEach( (question) => {
+    // console.log(question);
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', () => {
+        questions.forEach( (item) => {
+            if(item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+        question.classList.toggle('show-text');
     });
 });
+
+
+// traversing the dom:
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach( (btn) => {
+//     btn.addEventListener('click', (e) => {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
